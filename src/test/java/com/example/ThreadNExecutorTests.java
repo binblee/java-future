7deba756c84dbb33682eca1d160b60e9068a70b2
Li Bin <binblee@users.noreply.github.com>
2016-03-23 14:16:54 +0800

    thread and executor tests

---
+++ src/test/java/com/example/ThreadNExecutorTests.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by libin on 3/23/16.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(JavaFutureApplication.class)
+public class ThreadNExecutorTests {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
