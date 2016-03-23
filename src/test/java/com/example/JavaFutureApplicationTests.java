441ad11b9731240d30d03c74ade09a1893091856
Li Bin <binblee@users.noreply.github.com>
2016-03-23 14:08:02 +0800

    streams api

--- src/test/java/com/example/JavaFutureApplicationTests.java
+++ src/test/java/com/example/JavaFutureApplicationTests.java
@@ -11,8 +11,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = JavaFutureApplication.class)
@@ -90,6 +92,47 @@ public class JavaFutureApplicationTests {
 		Function<String, Integer> toInteger = Integer::valueOf;
 		Function<String, String> backToString = toInteger.andThen(String::valueOf);
 		Assert.assertEquals("0", backToString.apply("0"));
+	}
+
+	@Test
+	public void testStream(){
+		List<String> strings = Arrays.asList("bbb","aaa","ccc","aaa1");
+
+		strings.stream().filter((s) -> s.startsWith("aa"))
+			.forEach(System.out::println);
+
+		strings.stream().sorted()
+			.forEach(System.out::println);
+
+		strings.stream().map(String::toUpperCase)
+			.sorted()
+			.forEach(System.out::println);
+
+		boolean matched = strings.stream().allMatch((s) -> s.startsWith("a"));
+		Assert.assertEquals(false, matched);
+
+		matched = strings.stream().anyMatch((s)->s.startsWith("a"));
+		Assert.assertEquals(true, matched);
+
+		matched = strings.stream().noneMatch((s)->s.startsWith("a"));
+		Assert.assertEquals(false, matched);
+
+		long countOfStringsStartWithA = strings.stream()
+				.filter((s)->s.startsWith("a"))
+				.count();
+		Assert.assertEquals(2, countOfStringsStartWithA);
+
+		Optional<String> reducedStrings = strings.stream()
+				.sorted()
+				.reduce((s1,s2) -> s1 + ":" + s2);
+
+		reducedStrings.ifPresent(System.out::println);
+	}
+
+	@Test
+	public void testParallelStream(){
+		List<String> strings = Arrays.asList("1111","33","00","55");
+		strings.parallelStream().sorted().forEach(System.out::println);
 	}
 
 }
