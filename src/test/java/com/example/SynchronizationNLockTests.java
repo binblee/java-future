4398ba3ac749ac8723c3e23e0c7d6234fb4a6301
Li Bin <binblee@users.noreply.github.com>
2016-03-25 22:35:13 +0800

    synchroned method

--- src/test/java/com/example/SynchronizationNLockTests.java
+++ src/test/java/com/example/SynchronizationNLockTests.java
@@ -1,9 +1,15 @@
 package com.example;
 
+import com.example.concurrent.ConcurrentUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 /**
  * Created by libin on 3/25/16.
@@ -16,5 +22,32 @@ public class SynchronizationNLockTests {
     @Test
     public void contextLoad(){
 
+    }
+
+    private int count = 0;
+
+    public void increase(){
+        count += 1;
+    }
+
+    synchronized public void synchronizedIncrease(){
+        count += 1;
+    }
+
+
+    @Test
+    public void testWithoutSynchronization(){
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        IntStream.range(0,1000).forEach((i) -> executorService.submit(this::increase));
+        ConcurrentUtils.stop(executorService);
+        System.out.println(count);
+    }
+
+    @Test
+    public void testWithSynchronization(){
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        IntStream.range(0,1000).forEach(i -> executorService.submit(this::synchronizedIncrease));
+        ConcurrentUtils.stop(executorService);
+        Assert.assertEquals(1000, count);
     }
 }
