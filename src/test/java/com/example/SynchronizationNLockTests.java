8c5935fb6296fb8f532d3c4924ccb05104af8d87
Li Bin <binblee@users.noreply.github.com>
2016-03-25 22:15:16 +0800

    synchronization and lock tests

---
+++ src/test/java/com/example/SynchronizationNLockTests.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by libin on 3/25/16.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(JavaFutureApplication.class)
+public class SynchronizationNLockTests {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
