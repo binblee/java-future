be4eb4fbdbc6f344766c7f7a79a90dce7317b8b7
Li Bin <binblee@users.noreply.github.com>
2016-03-25 16:00:54 +0800

    executor invoke all

--- src/test/java/com/example/ThreadNExecutorTests.java
+++ src/test/java/com/example/ThreadNExecutorTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -76,5 +78,38 @@ public class ThreadNExecutorTests {
             System.out.println(e.getMessage());
         }
         System.out.println(result);
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void testFutureTimeout() throws Exception{
+        Callable<String> task = () -> {
+            TimeUnit.SECONDS.sleep(1);
+            return "should not see this result.";
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<String> future = executorService.submit(task);
+
+        String result = future.get(500,TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testInvokeAll() throws Exception {
+        List<Callable<String >> tasks = Arrays.asList(
+                () -> "task 1",
+                () -> "task 2",
+                () -> "task 3"
+        );
+
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        executorService.invokeAll(tasks).stream().map(
+                future -> {
+                    try {
+                        return future.get();
+                    }catch(Exception e){
+                        throw new IllegalStateException(e);
+                    }
+                }
+        ).forEach(System.out::println);
     }
 }
